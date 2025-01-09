36df43d87c5f6cd45cea408a9cd9b09aa123476e
WowMuchDoge <97766806+WowMuchDoge@users.noreply.github.com>
2025-01-08 18:18:16 -0600

    Initial token implementation

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
 
+#include "Token.hpp"
+
 int main() {
+    Token t(TokenType::AND, "and");
+
+    try {
+        double hehe = t.TokenToNumber();
+    } catch (const std::exception& e) {
+        std::cout << "Uh oh " << e.what() << std::endl;
+    }
 }
