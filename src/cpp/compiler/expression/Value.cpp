c9912c03e0bb1039ce11738608929fc1e72003e1
WowMuchDoge <97766806+WowMuchDoge@users.noreply.github.com>
2025-01-19 15:53:14 -0600

    Basic expression declaration and string removal

--- src/cpp/compiler/expression/Value.cpp
+++ src/cpp/compiler/expression/Value.cpp
@@ -1,22 +1,30 @@
 #include "compiler/expression/Value.hpp"
 
-#include <cstdlib>
-
 Value::Value() {
-    m_type = ValueType::NIL;
+	m_type = ValueType::NIL;
 }
 
-Value::Value(double v) {
-    m_type = ValueType::NUMBER;
-    m_value.number = v;
+void Value::SetNumber(double val) {
+	m_type = ValueType::NUMBER;
+	m_value = val;
 }
 
-Value::Value(bool b) {
-    m_type = ValueType::BOOL;
-    m_value.boolean = b;
+
+void Value::SetBoolean(bool b) {
+	m_type = ValueType::BOOL;
+	m_value = b;
 }
 
-// Value::Value(std::string s) {
-//     m_type = ValueType::STRING;
-//     m_value.string = s;
-// }
+
+void Value::SetString(std::string str) {
+	m_type = ValueType::STRING;
+	m_value = str;
+}
+
+void Value::SetNil() {
+	m_type = ValueType::NIL;
+}
+
+ValueType Value::GetType() {
+	return m_type;
+}
